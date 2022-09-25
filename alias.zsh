57c4ab91272b31826494d8f169bb5b5090df7230
Anthony Ongaro <43623494+ajongaro@users.noreply.github.com>
2022-09-25 11:04:03 -0700

    Update aliases

--- alias.zsh
+++ alias.zsh
@@ -6,10 +6,23 @@ alias t="take"
 alias up="cd .."
 alias home="~/documents"
 alias turing="cd ~/documents/turing/"
+alias mod1="cd ~/documents/turing/module1"
 alias aliases="atom ~/.oh-my-zsh/custom/alias.zsh"
 
 # Git stuff
 alias g="git"
+alias gc="git checkout"
+alias gc-="git checkout -"
+alias	gca="git commit -am"
+alias	gcb="git checkout -b"
+alias	ga="git add ."
+alias gp="git push -u origin main"
+alias	gl="git log --graph --oneline"
+alias	gs="git status"
+alias	gs="git stash"
+alias	gsp="git stash pop"
+alias	gss="git stash save"
+alias	gsl="git stash list"
 
 # Switch ls to exa
 alias l="exa --icons --sort=ext"
